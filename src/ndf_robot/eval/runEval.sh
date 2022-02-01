7b2ed648dcb3bd3827e45da6886c16563aa01302
elchun <etha.l.chun@gmail.com>
2022-02-01 15:49:04 -0500

    Add old to eval

--- src/ndf_robot/eval/runEval.sh
+++ src/ndf_robot/eval/runEval.sh
@@ -8,8 +8,8 @@ CUDA_VISIBLE_DEVICES=0 python evaluate_ndf_v2.py \
         --save_vis_per_model \
         --config eval_mug_gen \
         --exp test_mug_eval \
- 		--num_iterations 100 
-#		--use_gripper_occ 
+ 		--num_iterations 100 \
+ 		--use_gripper_occ 
 #       --pybullet_viz \
 #       --use_full_hand \
 #		--grasp_viz \
