ceab8bdbc176bb5cb36f3c4bb6c1f7b2fa00d764
Ethan L Chun <elchun@cronus.csail.mit.edu>
2022-03-01 20:57:15 -0500

    Add conv occ files

--- src/ndf_robot/model/conv_occupancy_net/conv_occupancy_net.py
+++ src/ndf_robot/model/conv_occupancy_net/conv_occupancy_net.py
@@ -1,5 +1,21 @@
+"""
+Based on implementation from: https://pengsongyou.github.io/conv_onet
+"""
 import torch
 import torch.nn as nn
+
+from ndf_robot.model.conv_occupancy_net.encoder import (
+    pointnet, voxels, pointnetpp
+)
+
+
+encoder_dict = {
+    'pointnet_local_pool': pointnet.LocalPoolPointnet,
+    'pointnet_crop_local_pool': pointnet.PatchLocalPoolPointnet,
+    'pointnet_plus_plus': pointnetpp.PointNetPlusPlus,
+    'voxel_simple_local': voxels.LocalVoxelEncoder,
+}
+
 
 class ConvolutionalOccupancyNetwork(nn.Module):
     """
